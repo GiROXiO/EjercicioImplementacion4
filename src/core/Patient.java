ca55fbfed0ed23dc4f2a393ca81b11882e5856f0
Jeronimo <jecastroam@gmail.com>
2025-04-02 11:25:08 -0500

    Segundo commit: Aniadir clases Team, Ward, Hospital y Patient. Se aniade Main

---
+++ src/core/Patient.java
@@ -0,0 +1,19 @@
+package core;
+
+import java.util.ArrayList;
+
+public class Patient {
+    private int id;
+    private Team team;
+    private Ward ward;
+    private ArrayList<Appoiment> appoiments;
+    private ArrayList<Doctor> doctors;
+
+    public Patient(int id, Team team, Ward ward) {
+        this.id = id;
+        this.team = team;
+        this.ward = ward;
+        this.appoiments = new ArrayList<>();
+        this.doctors = new ArrayList<>();
+    }
+}
--- src/core/Team.java
+++ src/core/Team.java
@@ -5,5 +5,13 @@ import java.util.ArrayList;
 public class Team {
     private int id;
     private ArrayList<Doctor> doctors;
-    private 
+    private ConsultantDoctor teamLeader;
+    private ArrayList<Patient> patients;
+
+    public Team(int id) {
+        this.id = id;
+        this.doctors = new ArrayList<>();
+        this.teamLeader = null;
+        this.patients = new ArrayList<>();
+    }
 }
